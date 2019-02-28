0be25dff0e0ef3aa05a3b3a2e0780db7811a4e7a
Stephen Wright <scw248@gmail.com>
2019-02-28 21:03:42 +0000

    Done.

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -6,6 +6,7 @@ class Helpers
   end
   
   def self.is_logged_in?(session)
-    User.find_by(:id => session[:user_id]) ? true : false
+    @result = !!self.current_user(session)
+    @result
   end
 end
